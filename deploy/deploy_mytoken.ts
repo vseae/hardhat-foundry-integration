11392c32d2c98d24973191a4d546439a6fc022eb
7Levy <15958732798@163.com>
2022-09-06 18:11:25 +0800

    update

--- deploy/deploy_mytoken.ts
+++ deploy/deploy_mytoken.ts
@@ -1,6 +1,5 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-
 const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
   const { deployer } = await getNamedAccounts();
