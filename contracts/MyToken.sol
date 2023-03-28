13dcaeadf61be193cf6293060fe53fbd55c33360
7Levy <15958732798@163.com>
2023-03-28 17:41:58 +0800

    no global import

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+pragma solidity ^0.8.0;
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor() ERC20("MyToken1", "MTK1") {
+    constructor() ERC20("MyToken", "MTK") {
         _mint(msg.sender, 1000 * 1e18);
     }
 }
