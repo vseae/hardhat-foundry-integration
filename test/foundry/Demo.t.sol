d87b0eabd8f7adb80d27b9be54f7ede2fcd38058
vseae <15958732798@163.com>
2023-05-22 17:22:16 +0800

    update

--- test/foundry/Demo.t.sol
+++ test/foundry/Demo.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {Demo} from "../../contracts/Demo.sol";
